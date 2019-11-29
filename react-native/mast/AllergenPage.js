ffabfb2086de117036c93ff6a84a9fb82850e8ef
Nishant Jha <me@nishantjha.org>
2019-11-29 13:02:37 -0500

    Simple navigation layout

---
+++ react-native/mast/AllergenPage.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+export default class SearchPage extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.title}>Allergen Page </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 65,
+  },
+});
+
+
+
