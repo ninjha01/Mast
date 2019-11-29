9e82aa0d8b227dddd6fc993d8af735e0e0d9237b
Nishant Jha <me@nishantjha.org>
2019-11-29 12:54:47 -0500

    switch to expo

---
+++ react-native/mast/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import SearchPage from "./SearchPage.js";
+
+const AppNavigator = createStackNavigator({
+  Search: SearchPage
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
