c57e8a0e91ec0537e41253615c7cea9f580a7f1d
Nishant Jha <me@nishantjha.org>
2019-11-29 19:04:09 -0500

    Simple Firebase Allergen fetch

--- react-native/mast/components/SearchPage/index.js
+++ react-native/mast/components/SearchPage/index.js
@@ -1,18 +1,69 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 
 export default class SearchPage extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      allergens: [],
+      loading: false,
+    }
+  }
+
+  getAllergens() {
+    this.setState({ loading: true });
+
+    fetch('https://mast-b0959.firebaseio.com/allergens.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          allergens: responseJson,
+          loading: false,
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          error: error
+        })
+        console.error(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getAllergens();
+  }
+
   render() {
+    if (this.state.error) {
+      return (
+        <Text style={styles.title}>this.state.error</Text>
+      )
+    }
+    if (this.state.loading) {
+      return (
+        <Text style={styles.title}>Loading</Text>
+      )
+    }
+    var allergens = this.state.allergens
+    if (allergens.length > 0) {
+      var allergenList = <FlatList
+        style={styles.container}
+        data={allergens}
+        keyExtractor={(item) => item.Name}
+        renderItem={({ item }) => <Text styles={styles.item}>{item.Name}</Text>}
+      />
+    }
+    else {
+      allergenList = <Text style={styles.item}> No Allergens Found </Text>
+    }
     return (
-      <View>
+      <View style={{ flex: 1 }} >
         <Text style={styles.title}>Search Page </Text>
-        <Button
-          title="Go to Allergen Page"
-          onPress={() => this.props.navigation.navigate("Allergen")}
-        />
-      </View>
+        {allergenList}
+      </View >
     );
   }
 }
@@ -22,6 +73,22 @@ const styles = StyleSheet.create({
     fontSize: 18,
     textAlign: 'center',
     marginTop: 65,
+  },
+  container: {
+    flex: 1,
+    marginTop: 65,
+    marginBottom: 65,
+    paddingTop: 22,
+    paddingBottom: 22,
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  item: {
+    padding: 20,
+    fontSize: 18,
+    height: 100,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
 
