f1bc1c5a6bd591b52cf6d50dec32e03254dda4ea
Nishant Jha <me@nishantjha.org>
2019-12-02 17:44:38 -0500

    AllergenPage shows all allergen fields

--- react-native/mast/components/AllergenPage/index.tsx
+++ react-native/mast/components/AllergenPage/index.tsx
@@ -1,21 +1,78 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
 
 export default class AllergenPage extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      allergen: this.props.navigation.getParam("allergen", {name: "unknown"})
-    }
+      allergen: this.props.navigation.getParam("allergen", { name: "unknown" })
+    };
+  }
+
+  renderRow(label, value) {
+    return (
+      <Card>
+        <Text>Hello World</Text>
+      </Card>
+    );
   }
 
   render() {
+    const allergen = this.state.allergen;
+    const list = [
+      {
+        label: "Name",
+        value: allergen.name
+      },
+      {
+        label: "Source",
+        value: allergen.source
+      },
+      {
+        label: "Order",
+        value: allergen.order
+      },
+      {
+        label: "Species",
+        value: allergen.species
+      },
+      {
+        label: "Biochemical Name",
+        value: allergen.biochemical_name
+      },
+      {
+        label: "MW (SDS-PAGE)",
+        value: allergen.mw
+      },
+      {
+        label: "Allergenicity",
+        value: allergen.allergenicity
+      },
+      {
+        label: "Allergenicity Ref",
+        value: allergen.allergenicity_ref
+      },
+      {
+        label: "Route",
+        value: allergen.route
+      }
+    ];
     return (
-      <View>
-        <Text style={styles.title}>{this.state.allergen.name}</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          {this.state.allergen.name} |#| {this.state.allergen.category}
+        </Text>
+        <ScrollView style={styles.list}>
+          {list.map(l => (
+            <Card>
+              <Text style={styles.label}>{l.label}</Text>
+              <Text style={styles.value}>{l.value}</Text>
+            </Card>
+          ))}
+        </ScrollView>
       </View>
     );
   }
@@ -24,8 +81,29 @@ export default class AllergenPage extends Component {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    textAlign: 'center',
-    marginTop: 65,
+    textAlign: "center",
+    marginTop: 65
   },
-  
+  container: {
+    flex: 1,
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginLeft: 16,
+    marginRight: 16
+  },
+  list: {
+    marginTop: 65,
+    marginBottom: 65,
+    marginLeft: 16,
+    marginRight: 16
+  },
+  label: {
+    fontWeight: "bold"
+  },
+  value: {
+    fontWeight: "300"
+  },
+  card: {
+    padding: 0
+  }
 });
