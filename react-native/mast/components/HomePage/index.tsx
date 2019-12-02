a03e006884ddddd7e31ad58b4b5347f570352933
Nishant Jha <me@nishantjha.org>
2019-12-02 18:56:59 -0500

    Add icon to HomePage

--- react-native/mast/components/HomePage/index.tsx
+++ react-native/mast/components/HomePage/index.tsx
@@ -1,17 +1,16 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React, { Component } from "react";
+import { Image, View, Text, StyleSheet, Button } from "react-native";
+import { SearchBar } from "react-native-elements";
 
 export default class HomePage extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       query: "",
       query_type: "name",
-      title: "Home Page",
+      title: "AllergenGuru"
     };
     this.updateSearch = this.updateSearch.bind(this);
     this.submitQuery = this.submitQuery.bind(this);
@@ -20,31 +19,35 @@ export default class HomePage extends Component {
   updateSearch(text) {
     this.setState({
       query: text
-    })
+    });
   }
 
   submitQuery() {
     this.props.navigation.navigate("Search", {
       query: this.state.query,
-      query_type: this.state.query_type,
-    })
+      query_type: this.state.query_type
+    });
   }
 
   render() {
     return (
-      <View style={styles.container} >
-        <Text style={styles.title}>{this.state.title}</Text>
-        <View style={styles.search}>
-          <SearchBar
-            lightTheme
-            round
-            searchIcon={{ size: 24 }}
-            onChangeText={this.updateSearch}
-            onSubmitEditing={this.submitQuery}
-            onClear={text => this.updateSearch('')}
-            placeholder="Search Allergens..."
-            value={this.state.query}
+      <View style={{ flex: 1 }}>
+        <SearchBar
+          lightTheme
+          round
+          searchIcon={{ size: 24 }}
+          onChangeText={this.updateSearch}
+          onSubmitEditing={this.submitQuery}
+          onClear={text => this.updateSearch("")}
+          placeholder="Search Allergens..."
+          value={this.state.query}
+        />
+        <View style={styles.container}>
+          <Image
+            style={styles.icon}
+            source={require("../../assets/splash_screen/logo.png")}
           />
+          <Text style={styles.title}>{this.state.title}</Text>
         </View>
       </View>
     );
@@ -54,16 +57,20 @@ export default class HomePage extends Component {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    textAlign: 'center',
-    marginTop: 65,
+    textAlign: "center",
+    marginTop: 65
   },
   container: {
     flex: 1,
+    padding: 32,
+    alignItems: "center"
   },
   search: {
-    marginTop: 16,
+    marginTop: 16
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    resizeMode: "contain"
   }
 });
-
-
-
