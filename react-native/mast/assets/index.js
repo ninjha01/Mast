6c5abd964725248cd7529d114b00316562843838
Nishant Jha <me@nishantjha.org>
2019-12-03 19:00:15 -0500

    HomePage icon layout

---
+++ react-native/mast/assets/index.js
@@ -0,0 +1,22 @@
+const images = {
+  common: {
+    logo: require("./splash_screen/logo.png")
+  },
+  home_page: {
+    icons: {
+      animal: require("./home_page/Icons/icon_animal.png"),
+      cockroach: require("./home_page/Icons/icon_cockroach.png"),
+      food: require("./home_page/Icons/icon_food.png"),
+      house_dust_mite: require("./home_page/Icons/icon_house_dust_mite.png"),
+      latex: require("./home_page/Icons/icon_latex.png"),
+      venom: require("./home_page/Icons/icon_venom.png"),
+      pollen: require("./home_page/Icons/icon_pollen.png"),
+      mold: require("./home_page/Icons/icon_mold.png"),
+      other: require("./home_page/Icons/icon_other.png")
+    }
+  },
+  search_page: {},
+  allergen_page: {}
+};
+
+export default images;
--- react-native/mast/components/HomePage/index.tsx
+++ react-native/mast/components/HomePage/index.tsx
@@ -1,8 +1,19 @@
 "use strict";
 
 import React, { Component } from "react";
-import { Image, View, Text, StyleSheet, Button } from "react-native";
+import {
+  TouchableOpacity,
+  Dimensions,
+  FlatList,
+  ScrollView,
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  Button
+} from "react-native";
 import { SearchBar } from "react-native-elements";
+import Images from "../../assets/";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -14,6 +25,7 @@ export default class HomePage extends Component {
     };
     this.updateSearch = this.updateSearch.bind(this);
     this.submitQuery = this.submitQuery.bind(this);
+    this.renderButton = this.renderButton.bind(this);
   }
 
   updateSearch(text) {
@@ -29,6 +41,23 @@ export default class HomePage extends Component {
     });
   }
 
+  renderButton(label, src) {
+    x;
+    return (
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.5}
+        onPress={() => {
+          this.setState({ query: label, query_type: "category" });
+          this.submitQuery();
+        }}
+      >
+        <Image source={src} style={styles.icon} />
+        <Text style={styles.label}>{label}</Text>
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
@@ -42,23 +71,32 @@ export default class HomePage extends Component {
           placeholder="Search Allergens..."
           value={this.state.query}
         />
+        <Text style={styles.title}>{this.state.title}</Text>
         <View style={styles.container}>
-          <Image
-            style={styles.icon}
-            source={require("../../assets/splash_screen/logo.png")}
-          />
-          <Text style={styles.title}>{this.state.title}</Text>
+          <Image style={styles.icon} source={Images.common.logo} />
         </View>
+        <ScrollView contentContainerStyle={styles.grid}>
+          <FlatList
+            data={buttons}
+            renderItem={({ item }) =>
+              this.renderButton(item.label, item.imageSrc)
+            }
+            numColumns={3}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </ScrollView>
       </View>
     );
   }
 }
 
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     textAlign: "center",
-    marginTop: 65
+    marginTop: 30
   },
   container: {
     flex: 1,
@@ -69,8 +107,63 @@ const styles = StyleSheet.create({
     marginTop: 16
   },
   icon: {
-    width: 60,
-    height: 60,
-    resizeMode: "contain"
+    height: 0.1 * height,
+    resizeMode: "contain",
+    alignItems: "center",
+    padding: 8
+  },
+  label: {
+    fontSize: 0.03 * width,
+    textAlign: "center",
+    padding: 8
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+    padding: 8
+  },
+  grid: {
+    paddingLeft: 32,
+    paddingRight: 32,
+    height: 0.5 * height
   }
 });
+
+const buttons = [
+  {
+    label: "Animal",
+    imageSrc: Images.home_page.icons.animal
+  },
+  {
+    label: "Cockroach",
+    imageSrc: Images.home_page.icons.cockroach
+  },
+  {
+    label: "Food",
+    imageSrc: Images.home_page.icons.food
+  },
+  {
+    label: "House Dust Mite",
+    imageSrc: Images.home_page.icons.house_dust_mite
+  },
+  {
+    label: "Mold",
+    imageSrc: Images.home_page.icons.mold
+  },
+  {
+    label: "Latex",
+    imageSrc: Images.home_page.icons.latex
+  },
+  {
+    label: "Venom",
+    imageSrc: Images.home_page.icons.venom
+  },
+  {
+    label: "Pollen",
+    imageSrc: Images.home_page.icons.pollen
+  },
+  {
+    label: "Other",
+    imageSrc: Images.home_page.icons.other
+  }
+];
