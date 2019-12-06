714821991864585bef4179d7ce4ddeac62fb2c7b
Nishant Jha <me@nishantjha.org>
2019-12-06 06:42:42 -0500

    post clone script typo fix

--- react-native/mast/appcenter-post-clone.sh
+++ react-native/mast/appcenter-post-clone.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 if [ -z "$FIREBASE_API_KEY" ];
-   # load firebase api from appcenter/github secrets
-   # so that react-native-dotenv can expose itcase 
-   echo "$FIREBASE_API_KEY" > .env 
+then
+    # load firebase api from appcenter/github secrets
+    # so that react-native-dotenv can expose itcase 
+    echo "$FIREBASE_API_KEY" > .env
+fi;
 npm install
 yarn install
 cd ios/
 pod install && pod install --repo-update # unsure if both neccesary
+
