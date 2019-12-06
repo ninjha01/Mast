8bb2478aabb8fa9076f07147d29ab2e59c4c32bd
Nishant Jha <ninjha01@gmail.com>
2019-12-06 07:26:22 -0500

    debug post clone

--- react-native/mast/appcenter-post-clone.sh
+++ react-native/mast/appcenter-post-clone.sh
@@ -2,7 +2,8 @@
 if [ -z "$FIREBASE_API_KEY" ];
 then
     # load firebase api from appcenter/github secrets
-    # so that react-native-dotenv can expose itcase 
+    # so that react-native-dotenv can expose itcase
+    echo "Setting firebase key"
     echo "$FIREBASE_API_KEY" > .env
 fi;
 npm install
