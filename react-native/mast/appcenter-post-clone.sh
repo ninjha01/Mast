fe77d8853373fd1f7c5255a5b2b66ed4375ae8ae
Nishant Jha <me@nishantjha.org>
2019-12-06 06:22:14 -0500

    Push firebase api key set during build (appcenter/github secrets)

--- react-native/mast/appcenter-post-clone.sh
+++ react-native/mast/appcenter-post-clone.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -z "$FIREBASE_API_KEY" ];
+   # load firebase api from appcenter/github secrets
+   # so that react-native-dotenv can expose itcase 
+   echo "$FIREBASE_API_KEY" > .env 
 npm install
 yarn install
 cd ios/
