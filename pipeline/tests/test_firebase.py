4ddbca50700837fa473269da17a9034c4c5f63fa
Nishant Jha <ninjha01@gmail.com>
2020-01-22 20:16:57 -0500

    import hack to get cicd tests to work

--- pipeline/tests/test_firebase.py
+++ pipeline/tests/test_firebase.py
@@ -1,10 +1,12 @@
-from database.firebase import firestore
 import uuid
 import pytest
 
 
 @pytest.mark.needs_auth
 def test_write_firestore():
+    # HACK: Moving import here to pass cicd test suite
+    from database.firebase import firestore
+
     store = firestore.client()
     rand_id = str(uuid.uuid4())
     rand_val = {"test_val": str(uuid.uuid4())}
