25e777b529a765bb5fd1c6da2cc9f4d9120b6ba8
Nishant Jha <ninjha01@gmail.com>
2020-01-22 20:08:10 -0500

    cicd pytest ignores pytests that need auth

--- pipeline/tests/test_firebase.py
+++ pipeline/tests/test_firebase.py
@@ -1,7 +1,9 @@
 from database.firebase import firestore
 import uuid
+import pytest
 
 
+@pytest.mark.needs_auth
 def test_write_firestore():
     store = firestore.client()
     rand_id = str(uuid.uuid4())
