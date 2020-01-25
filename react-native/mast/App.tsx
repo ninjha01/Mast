2cc6d357189ebb242bfbefc8aadaab6874e2b1f6
Nishant Jha <me@nishantjha.org>
2020-01-25 12:34:15 -0500

    install navigation drawer

--- react-native/mast/App.tsx
+++ react-native/mast/App.tsx
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { ThemeProvider } from 'react-native-elements';
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { ThemeProvider } from "react-native-elements";
 
 import HomePage from "./components/HomePage";
 import SearchPage from "./components/SearchPage";
 import AllergenPage from "./components/AllergenPage";
 
-const AppNavigator = createStackNavigator({
+const MainStackNavigator = createStackNavigator({
   Home: HomePage,
   Search: SearchPage,
   Allergen: AllergenPage
 });
 
-const AppContainer = createAppContainer(AppNavigator);
+const DrawerNavigator = createDrawerNavigator({
+  MainStack: {
+    screen: MainStackNavigator
+  }
+});
+
+const AppContainer = createAppContainer(DrawerNavigator);
 
 export default class App extends Component {
   render() {
@@ -21,6 +28,6 @@ export default class App extends Component {
       <ThemeProvider>
         <AppContainer />
       </ThemeProvider>
-    )
+    );
   }
 }
