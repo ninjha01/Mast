f7aa9f3a5c857eb8e731666e893e87cd6921f599
Nishant Jha <ninjha01@gmail.com>
2020-02-04 07:58:30 -0500

    track Pods

--- react-native/mast/appcenter-post-clone.sh
+++ react-native/mast/appcenter-post-clone.sh
@@ -6,8 +6,3 @@ then
     echo "Setting firebase key"
     echo "FIREBASE_API_KEY=$USER_DEFINED_FIREBASE_API_KEY" > .env
 fi;
-npm install
-yarn install
-cd ios/
-pod repo update
-pod install && pod install --repo-update # unsure if both neccesary
